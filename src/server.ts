add3d989ec7c65453001e406e0daa3be709f7553
proloypaul <proloyp23@gmail.com>
2023-05-29 21:02:28 +0600

    [DONE] setup express server

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config/index";
+async function main() {
+
+    try{
+        // await mongoose.connect('mongodb://127.0.0.1:27017/test');
+        await mongoose.connect(config.database_url as string);
+        console.log("Database connected successfully")
+
+        app.listen(config.port, () => {
+            console.log(`University management server running port:  ${config.port}`)
+        })
+    }catch(err){
+        console.log("Failed to connect database ", err)
+    }
+    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+  }
+
+main()
