f5bb6be504b85580dc535d8a94499da29db5cf12
proloypaul <proloyp23@gmail.com>
2023-06-03 13:28:41 +0600

    Manage log according to folder

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,18 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config/index'
+import { logger, loggerError } from './shared/logger'
 async function main() {
   try {
     // await mongoose.connect('mongodb://127.0.0.1:27017/test');
     await mongoose.connect(config.database_url as string)
-    console.log('Database connected successfully')
+    logger.info('Database connected successfully')
 
     app.listen(config.port, () => {
-      console.log(`University management server running port:  ${config.port}`)
+      logger.info(`University management server running port:  ${config.port}`)
     })
   } catch (err) {
-    console.log('Failed to connect database ', err)
+    loggerError.error('Failed to connect database ', err)
   }
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
