2fa053c316e3175cbdab3d480cbf2d13e07550d9
proloypaul <proloyp23@gmail.com>
2023-06-09 00:50:28 +0600

    optimize res json file

--- src/app/modules/AcamedicSemester/academicSemester.controller.ts
+++ src/app/modules/AcamedicSemester/academicSemester.controller.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
 import { academicSemesterService } from './academicSemester.service';
 import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
+import { StatusCodes } from 'http-status-codes';
 
 const createAcademicSemester = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -9,11 +11,18 @@ const createAcademicSemester = catchAsync(
       academicSemesterData
     );
     next();
-    res.status(200).json({
+
+    sendResponse(res, {
+      statusCode: StatusCodes.OK,
       success: true,
       message: 'Academic data created successfully',
       data: result,
     });
+    // res.status(200).json({
+    //   success: true,
+    //   message: 'Academic data created successfully',
+    //   data: result,
+    // });
   }
 );
 
--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -1,13 +1,24 @@
 import { UserService } from './user.service';
 import catchAsync from '../../../shared/catchAsync';
 import { NextFunction, Request, Response } from 'express';
+import sendResponse from '../../../shared/sendResponse';
+import { StatusCodes } from 'http-status-codes';
 
 const createUserToDB = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const { userData } = req.body;
     const result = await UserService.createUser(userData);
     next();
-    res.status(200).json({
+
+    // res.status(200).json({
+    //   success: true,
+    //   message: 'User created successfully',
+    //   data: result,
+    // });
+
+    // optimize code
+    sendResponse(res, {
+      statusCode: StatusCodes.OK,
       success: true,
       message: 'User created successfully',
       data: result,
