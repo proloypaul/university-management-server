b738d0429ac3eb52f981a233018e8e6a5fe84621
proloypaul <proloyp23@gmail.com>
2023-07-13 18:33:26 +0600

    create auth middlerware

---
+++ src/app/middleware/auth.ts
@@ -0,0 +1,35 @@
+import { NextFunction, Request, Response } from 'express';
+import { Secret } from 'jsonwebtoken';
+import ApiError from '../../errors/ApiError';
+import { StatusCodes } from 'http-status-codes';
+import { jwtHelpers } from '../../helpers/jwtHelpers';
+import config from '../../config';
+
+const auth =
+  (...requiredRoles: string[]) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      //get authorization token
+      const token = req.headers.authorization;
+      console.log('token', token);
+      if (!token) {
+        throw new ApiError(StatusCodes.UNAUTHORIZED, 'You are not authorized');
+      }
+      // verify token
+      let verifiedUser = null;
+
+      verifiedUser = jwtHelpers.verifyToken(token, config.jwt.secret as Secret);
+
+      req.user = verifiedUser; // role  , userid
+
+      // role diye guard korar jnno
+      if (requiredRoles.length && !requiredRoles.includes(verifiedUser.role)) {
+        throw new ApiError(StatusCodes.FORBIDDEN, 'Forbidden');
+      }
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+
+export default auth;
--- src/app/modules/AcademicFaculty/academicFaculty.router.ts
+++ src/app/modules/AcademicFaculty/academicFaculty.router.ts
@@ -2,22 +2,51 @@ import express from 'express';
 import { AcademicFacultyController } from './academicFaculty.controller';
 import { AcadmeicFacultyValidation } from './academicFaculty.validation';
 import validateRequest from '../../middleware/validateRequest';
+import auth from '../../middleware/auth';
+import { ENUM_USER_ROLE } from '../../../enums/user';
 
 const router = express.Router();
 
 router.post(
   '/create-academic-faculty',
   validateRequest(AcadmeicFacultyValidation.createAcademicFacultyZodSchema),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   AcademicFacultyController.createAcademicFaculty
 );
 
-router.get('/:id', AcademicFacultyController.getSingleAcademicFaculty);
+router.get(
+  '/:id',
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.FACULTY
+  ),
+  AcademicFacultyController.getSingleAcademicFaculty
+);
 router.patch(
   '/:id',
   validateRequest(AcadmeicFacultyValidation.updateAcademicSemesterZodSchema),
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.FACULTY
+  ),
   AcademicFacultyController.updateAcademicFaculty
 );
-router.delete('/:id', AcademicFacultyController.deleteAcademicFaculty);
-router.get('/', AcademicFacultyController.getAllAcademicFaculty);
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN),
+  AcademicFacultyController.deleteAcademicFaculty
+);
+router.get(
+  '/',
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.FACULTY,
+    ENUM_USER_ROLE.STUDENT
+  ),
+  AcademicFacultyController.getAllAcademicFaculty
+);
 
 export const academicFacultyRouters = router;
